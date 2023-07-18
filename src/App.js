43ba44c010723e4feec40b99036bba121efb0877
soudomsinh <sengdaosinh@gmail.com>
2023-07-18 16:05:17 -0400

    My project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Cards from './Cards';
+import Navbar from './Navbar';
+import React,  {useState, useEffect} from 'react';
 
 function App() {
+  const [data, setData] = useState([])
+  useEffect(()=>{
+    fetch('https://www.melivecode.com/api/attractions')
+    .then(res=>res.json())
+    .then((result =>{
+      setData(result)}))
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar />
+        <h1 className='main-title' >Top Tourist Destination</h1>
+        <div className='grid-container'>
+          {data.map((item)=>
+            <Cards  
+            key= {item.id}
+            name = {item.name} 
+            coverimage = {item.coverimage} 
+            detail={item.detail} />
+          )}
+
+        </div>
     </div>
   );
 }
